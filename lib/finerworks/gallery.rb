768d6fc40281b2155fbbca9a74931281658df436
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2015-09-13 16:52:06 -0400

    add question mark method alias

--- lib/finerworks/gallery.rb
+++ lib/finerworks/gallery.rb
@@ -5,5 +5,7 @@ module FinerWorks
     property :title, from: "GalleryTitle"
     property :active, from: "GalleryActive", transform_with: -> (value) { value == "True" }
     property :guid, from: "GalleryGUID"
+
+    alias_method :active?, :active
   end
 end
