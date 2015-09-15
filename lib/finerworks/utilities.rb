12553498a8d35bcaefe9d0f51612893cc1dd6df1
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2015-09-15 13:09:54 -0400

    factor DST into TZ workaround

--- lib/finerworks/utilities.rb
+++ lib/finerworks/utilities.rb
@@ -7,7 +7,11 @@ module FinerWorks
     # Parse ISO 8601 time strings from the FinerWorks Web API. This function takes into consideration the fact that
     # the FinerWorks API does not specify a time zone in its responses, yet it appears to use US Central Time.
     def parse_api_time(time_string)
-      time_string += "-06:00" if time_string.split("T").last.split(/[+-]/).count < 2
+      if time_string.split("T").last.split(/[+-]/).count < 2
+        # If no TZ given, figure out if it's CST or CDT.
+        offset = Time.parse(time_string).dst? ? "-05:00" : "-06:00"
+        time_string += offset
+      end
       Time.parse(time_string)
     end
   end
--- test/finerworks/test_account.rb
+++ test/finerworks/test_account.rb
@@ -8,7 +8,7 @@ class TestAccount < Minitest::Test
   # Verify that Account correctly initializes its attributes from the Web API.
   def test_attributes
     assert_equal "Milton", @account.first_name
-    assert_equal Time.new(2015, 9, 13, 0, 0, 0, "-06:00"), @account.registration_date
+    assert_equal Time.new(2015, 9, 13, 0, 0, 0, "-05:00"), @account.registration_date
     assert_equal "", @account.middle_name
   end
 end
