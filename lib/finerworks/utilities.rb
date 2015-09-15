31a7baa359cb6a2a90c0ce5e1cb7408ce52c3aae
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2015-09-14 20:26:25 -0400

    check for existing TZ in time parser

--- lib/finerworks/utilities.rb
+++ lib/finerworks/utilities.rb
@@ -5,9 +5,10 @@ module FinerWorks
     module_function
 
     # Parse ISO 8601 time strings from the FinerWorks Web API. This function takes into consideration the fact that
-    # the FinerWorks API does not specify a time zone, but appears to use US Central Time.
+    # the FinerWorks API does not specify a time zone in its responses, yet it appears to use US Central Time.
     def parse_api_time(time_string)
-      Time.parse(time_string + "-06:00")
+      time_string += "-06:00" if time_string.split("T").last.split(/[+-]/).count < 2
+      Time.parse(time_string)
     end
   end
 end
--- test/finerworks/test_utilities.rb
+++ test/finerworks/test_utilities.rb
@@ -5,5 +5,6 @@ class TestUtilities < Minitest::Test
   def test_parse_api_time
     assert_equal Time.new(2015, 9, 13, 21, 0, 05, "-06:00"), FinerWorks::Utilities.parse_api_time("2015-09-13T21:00:05-06:00")
     assert_equal Time.new(2015, 9, 13, 0, 0, 0, "-06:00"), FinerWorks::Utilities.parse_api_time("2015-09-13T00:00:00-06:00")
+    assert_equal Time.new(2015, 9, 13, 21, 0, 05, "-06:00"), FinerWorks::Utilities.parse_api_time("2015-09-13T21:00:05")
   end
 end
